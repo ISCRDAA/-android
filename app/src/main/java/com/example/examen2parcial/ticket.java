a66f198604dba9d92e1432970fa4e19dd2e26cb8
Angel Alonso Rodriguez <rdangellons@gmail.com>
2022-11-11 01:22:23 -0600

    ticket

---
+++ app/src/main/java/com/example/examen2parcial/ticket.java
@@ -0,0 +1,15 @@
+package com.example.examen2parcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ticket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket);
+    }
+
+}
